4ac998b952a3468b2c4e0133250329bf84fead4e
DavidFranco3 <josedavidayalafranco3@gmail.com>
2023-06-06 12:13:11 -0600

    correcciones

--- src/page/Rendimientos/index.jsx
+++ src/page/Rendimientos/index.jsx
@@ -178,6 +178,7 @@ function Rendimientos({ setRefreshCheckLogin, location, history }) {
         folio,
         fichaSocio,
         rendimiento,
+        periodo: periodo,
         tipo: razon,
         createdAt: earningsDate,
       }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,7 +7,7 @@ export const API_HOST = 'https://isotech.mx/api-condumex';
 // export const API_CLOUDINARY = "https://api.cloudinary.com/v1_1/omarlestrella/image/upload";
 
 // API para la consulta de la IP pública
-// export const  API_IP = "https://obten-ip-api.herokuapp.com/"
+// export const  API_IP = "https://obten-ip-api.herokuapp.com"
 
 export const TOKEN = 'token-caja';
 
