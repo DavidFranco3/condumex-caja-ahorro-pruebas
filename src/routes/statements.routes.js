75dfdf13c646fe9b85c2c8063d4e2304da37949c
David Franco <josedavidayalafranco3@gmail.com>
2024-05-06 18:26:23 +0000

    Update statements.routes.js

--- src/routes/statements.routes.js
+++ src/routes/statements.routes.js
@@ -391,7 +391,7 @@ router.get("/email/:fichaSocio", verifyToken, async (req, res) => {
       secure: false,
       auth: {
         user: "ca.condutel@condumex.com.mx",
-        pass: "cah03.2024",
+        pass: "cah05.2024",
       },
     });
 
