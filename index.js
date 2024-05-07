8038f53b9eb5721f6471fa8ce71de2ae33497dff
DavidFranco <josedavidayalafranco3@gmail.com>
2024-05-07 13:50:12 -0600

    correcciones

--- index.js
+++ index.js
@@ -131,4 +131,4 @@ const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-module.exports = { server, app };
+module.exports = app;
--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 // const URI = "mongodb://127.0.0.1/caja-de-ahorro";
 
 // Credenciales para conexion a Mongo Atlas
-const URI = "mongodb+srv://david:rog8vhGyOyYrI6nA@cluster0.dhmepvm.mongodb.net/sample_mflix";
+const URI = "mongodb+srv://david:rog8vhGyOyYrI6nA@cluster0.dhmepvm.mongodb.net/caja-de-ahorro";
 
 mongoose.set('strictQuery', true);
 
