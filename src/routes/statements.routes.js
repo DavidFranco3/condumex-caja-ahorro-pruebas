6f8f5e3f5b2f539ee2822adcf5bcf35df2db01c0
José David Ayala Franco <73260715+DavidFranco3@users.noreply.github.com>
2024-11-06 09:16:01 -0600

    Update statements.routes.js

--- src/routes/statements.routes.js
+++ src/routes/statements.routes.js
@@ -391,7 +391,7 @@ router.get("/email/:fichaSocio", verifyToken, async (req, res) => {
       secure: false,
       auth: {
         user: "ca.condutel@condumex.com.mx",
-        pass: "cah08.2024",
+        pass: "cah11.2024",
       },
     });
 
