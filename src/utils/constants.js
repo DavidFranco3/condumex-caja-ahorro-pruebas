2b0815d11d15abcc46fe9724f0071799404275a8
José David Ayala Franco <josedavidayalafranco3@gmail.com>
2024-11-15 16:21:12 -0600

    ajustes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 // Constante para desarrollo
 // export const API_HOST = 'http://localhost:5050';
 
-// export const API_HOST = 'https://condumex.isotech.mx/back';
+export const API_HOST = 'https://condumex.isotech.mx/back';
 
 // export const API_CLOUDINARY = "https://api.cloudinary.com/v1_1/omarlestrella/image/upload";
 
